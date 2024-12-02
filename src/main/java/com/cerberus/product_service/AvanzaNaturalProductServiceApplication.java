e4c23db350a80d5ddd4e0a6e5bdea77a5a036439
egor <egormalov.2007@gmail.com>
2024-12-02 20:46:45 +1000

    Initial commit

---
+++ src/main/java/com/cerberus/product_service/AvanzaNaturalProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cerberus.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzaNaturalProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvanzaNaturalProductServiceApplication.class, args);
+    }
+
+}
